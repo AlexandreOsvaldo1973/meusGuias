df64a3438c1aa2486fe60f1d2065098099de81e4
Alexandre Osvaldo <alexandreosvaldo@hotmail.com>
2022-12-29 11:41:37 -0300

    Tutoriais com o básico de cada tecnologia

---
+++ tutorial.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php echo "<P>Olá Mundo!</p>"; ?>
+	<?php phpinfo(); ?>
+</body>
+</html>
